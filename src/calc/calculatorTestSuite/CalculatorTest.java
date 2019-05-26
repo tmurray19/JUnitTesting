d9e1a13e1c7a73df8856d29bc8694a765546ddbd
tmurray19 <me@taidghmurray.ie>
2019-05-26 15:02:43 +0100

    Ignoring tests.

--- src/calc/calculatorTestSuite/CalculatorTest.java
+++ src/calc/calculatorTestSuite/CalculatorTest.java
@@ -1,7 +1,9 @@
 package calc.calculatorTestSuite;
 
+import static org.junit.Assert.*;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class CalculatorTest {
@@ -17,9 +19,13 @@ public class CalculatorTest {
     public static void tearDownClass() {
     }
 
+    
+    // Ignoring tests
+    // We can also add messages explaining the ignore
+    @Ignore("I don't want to test this method right now, I know the outcome of it.")
     @Test
     public void testMain() {
-   
+    	assertFalse(true);
 
     }
     
